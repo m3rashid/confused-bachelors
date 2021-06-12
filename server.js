7f772c955d595661a4c666d0eb8a378f4f4a2538
m3rashid <mdrashid.hussain.786.01@gmail.com>
2021-06-12 20:33:23 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+// packages -- express body-parser ejs mongoose passport express-session passport-local passport-local-mongoose dotenv marked jsdom dompurify lodash
+
+// express -- firing up the server
+// body-parser -- taking form inputs
+// ejs -- creating view engine
+// passport, passport-local, passport-local-mongoose and express-session -- web session and authentication
+// marked -- create markdown from blog form input
+// jsdom -- creating virtual dom to render the markedown
+// dompurify -- sanitizing the HTML to make sure no scripts are executed in the form body
+
+// lodash -- formatting text for slugs or titles (use slug package instead)
+// slug -- to maintain the uniqueness of URL by adding URL slug at the end
