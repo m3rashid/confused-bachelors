c750fd69d0a3ab09e395af437946576d531044fe
Sarfraz-droid <73013838+Sarfraz-droid@users.noreply.github.com>
2021-06-16 14:29:07 +0530

    wirters

--- static/js/index.js
+++ static/js/index.js
@@ -68,17 +68,6 @@ $('.owl-carousel').owlCarousel({
     margin: -10,
     nav: true,
     dots: false,
-    // responsive: {
-    //     0: {
-    //         items: 1
-    //     },
-    //     600: {
-    //         items: 3
-    //     },
-    //     1000: {
-    //         items: 5
-    //     }
-    // }
 })
 
 var carouselitem = document.querySelectorAll(".owl-item");
