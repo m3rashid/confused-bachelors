4858fb96cca41b7262c6636b720d08a0df2dcd7e
Sarfraz-droid <73013838+Sarfraz-droid@users.noreply.github.com>
2021-06-16 13:08:37 +0530

    Carousel

--- static/js/index.js
+++ static/js/index.js
@@ -67,17 +67,18 @@ document.addEventListener("DOMContentLoaded", function() {
 $('.owl-carousel').owlCarousel({
     margin: -10,
     nav: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
+    dots: false,
+    // responsive: {
+    //     0: {
+    //         items: 1
+    //     },
+    //     600: {
+    //         items: 3
+    //     },
+    //     1000: {
+    //         items: 5
+    //     }
+    // }
 })
 
 var carouselitem = document.querySelectorAll(".owl-item");
